a3c267f6a1299438dc71e74641c59ba3f50c510e
Tian(Sophie）Xie <34063461+SophieCXT@users.noreply.github.com>
2021-04-29 19:57:06 -0400

    reformat the file

--- do_q_learning.py
+++ do_q_learning.py
@@ -1,11 +1,12 @@
 import gym
 import numpy as np
 from gym import spaces, envs
-from envs.simulator import NetworkSimulatorEnv
-from agents.q_agent import networkTabularQAgent
+from simulator import NetworkSimulatorEnv
+from q_agent import networkTabularQAgent
+
 
 ####
-# This script currently makes the agent use the a random policy to explor the state,action space
+# This script currently makes the agent use the a random policy to explore the state,action space
 # then tests the value of the learning by every 1000 iterations using the best choice and printing the reward
 ####
 
@@ -32,18 +33,19 @@ def main():
                     reward, next_state = env.pseudostep(action)
                     agent.learn(current_state, next_state, reward, action, done, env.nlinks)
 
-                action  = agent.act(current_state, env.nlinks)
+                action = agent.act(current_state, env.nlinks)
                 state_pair, reward, done, _ = env.step(action)
 
                 next_state = state_pair[0]
                 agent.learn(current_state, next_state, reward, action, done, env.nlinks)
                 r_sum_random += reward
 
-                if t%10000 == 0:
+                if t % 10000 == 0:
 
                     if env.routed_packets != 0:
-                        print "q learning with callmean:{} time:{}, average delivery time:{}, length of average route:{}, r_sum_random:{}".format(i, t, float(env.total_routing_time)/float(env.routed_packets), float(env.total_hops)/float(env.routed_packets), r_sum_random)
-
+                        print "q learning with callmean:{} time:{}, average delivery time:{}, length of average " \
+                              "route:{}, r_sum_random:{}".format(i, t, float(env.total_routing_time) / float(
+                            env.routed_packets), float(env.total_hops) / float(env.routed_packets), r_sum_random)
 
                     current_state = state_pair[1]
                     n = current_state[0]
@@ -53,7 +55,7 @@ def main():
                         reward, next_state = env.pseudostep(action)
                         agent.learn(current_state, next_state, reward, action, done, env.nlinks)
 
-                    action  = agent.act(current_state, env.nlinks, True)
+                    action = agent.act(current_state, env.nlinks, True)
                     state_pair, reward, done, _ = env.step(action)
 
                     next_state = state_pair[0]
@@ -61,8 +63,9 @@ def main():
                     r_sum_best += reward
 
                     if env.routed_packets != 0:
-                        print "q learning with callmean:{} time:{}, average delivery time:{}, length of average route:{}, r_sum_best:{}".format(i, t, float(env.total_routing_time)/float(env.routed_packets), float(env.total_hops)/float(env.routed_packets), r_sum_best)
-
+                        print "q learning with callmean:{} time:{}, average delivery time:{}, length of average route:{}, r_sum_best:{}".format(
+                            i, t, float(env.total_routing_time) / float(env.routed_packets),
+                            float(env.total_hops) / float(env.routed_packets), r_sum_best)
 
 
 if __name__ == '__main__':
--- q_agent.py
+++ q_agent.py
@@ -2,6 +2,7 @@ import collections
 import numpy as np
 from random import random
 
+
 class networkTabularQAgent(object):
     """
     Agent implementing tabular Q-learning for the NetworkSimulatorEnv.
@@ -9,22 +10,20 @@ class networkTabularQAgent(object):
 
     def __init__(self, num_nodes, num_actions, distance, nlinks):
         self.config = {
-            "init_mean" : 0.0,      # Initialize Q values with this mean
-            "init_std" : 0.0,       # Initialize Q values with this standard deviation
-            "learning_rate" : 0.7,
-            "eps": 0.1,            # Epsilon in epsilon greedy policies
+            "init_mean": 0.0,  # Initialize Q values with this mean
+            "init_std": 0.0,  # Initialize Q values with this standard deviation
+            "learning_rate": 0.7,
+            "eps": 0.1,  # Epsilon in epsilon greedy policies
             "discount": 1,
-            "n_iter": 10000000}        # Number of iterations
-        self.q = np.zeros((num_nodes,num_nodes,num_actions))
+            "n_iter": 10000000}  # Number of iterations
+        self.q = np.zeros((num_nodes, num_nodes, num_actions))
 
         for src in range(num_nodes):
             for dest in range(num_nodes):
                 for action in range(nlinks[src]):
                     self.q[src][dest][action] = distance[src][dest]
 
-
-
-    def act(self, state, nlinks,  best=False):
+    def act(self, state, nlinks, best=False):
         n = state[0]
         dest = state[1]
 
@@ -32,14 +31,13 @@ class networkTabularQAgent(object):
             best = self.q[n][dest][0]
             best_action = 0
             for action in range(nlinks[n]):
-                if self.q[n][dest][action] < best:  #+ eps:
+                if self.q[n][dest][action] < best:  # + eps:
                     best = self.q[n][dest][action]
                     best_action = action
         else:
             best_action = int(np.random.choice((0.0, nlinks[n])))
 
         return best_action
-
 
     def learn(self, current_event, next_event, reward, action, done, nlinks):
 
@@ -54,5 +52,6 @@ class networkTabularQAgent(object):
             if self.q[n_next][dest][link] < future:
                 future = self.q[n_next][dest][link]
 
-        #Q learning
-        self.q[n][dest][action] += (reward + self.config["discount"]*future - self.q[n][dest][action])* self.config["learning_rate"]
+        # Q learning
+        self.q[n][dest][action] += (reward + self.config["discount"] * future - self.q[n][dest][action]) * self.config[
+            "learning_rate"]
